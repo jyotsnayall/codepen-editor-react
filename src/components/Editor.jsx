5207114085797f6fb1107baa17c32eaf3d949a31
jyotsnayall <jyotsnasharma2603@gmail.com>
2023-08-25 22:48:15 +0530

    added open-collapse buttons on editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import '../index.css'
 import useLocalStorage from './storage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Editor() {
     const [html, setHtml] = useLocalStorage('html', '');
     const [css, setCss] = useLocalStorage('css', '');
     const [js, setJs] = useLocalStorage('js', '');
     const [srcCode, setSrcCode] = useState('');
+
+    const [openHtml, setOpenHtml] = useState(true)
+    const [openCss, setOpenCss] = useState(true)
+    const [openJs, setOpenJs] = useState(true)
 
     useEffect(() => {
         const timeout = setTimeout(() => {
@@ -29,20 +35,46 @@ function Editor() {
             </div>
 
             <div className='input-cover'>
-                <div className='editor-cover'>
-                    <span className='editor-title'>HTML</span>
+                <div className={`editor-cover ${openHtml ? '' : 'html-collapsed'}`}>
+                    <div className='editor-title'>
+                        HTML
+                        <button
+                            type="button"
+                            className="expand-collapse-btn"
+                            onClick={() => setOpenHtml(prevOpen => !prevOpen)}
+                        >
+                            <FontAwesomeIcon icon={openHtml ? faCompressAlt : faExpandAlt} />
+                        </button>
+                    </div>
                     <textarea value={html} type='text' placeholder='HTML' className='input' onChange={(e) => setHtml(e.target.value)} />
                 </div>
-                
+
                 <div className='width' />
-                <div className='editor-cover'>
-                    <span className='editor-title'>CSS</span>
+                <div className={`editor-cover ${openCss ? '' : 'css-collapsed'}`}>
+                    <div className='editor-title'>CSS
+                        <button
+                            type="button"
+                            className="expand-collapse-btn"
+                            onClick={() => setOpenCss(prevOpen => !prevOpen)}
+                        >
+                            <FontAwesomeIcon icon={openCss ? faCompressAlt : faExpandAlt} />
+                        </button>
+                    </div>
                     <textarea value={css} type='text' placeholder='CSS' className='input' onChange={(e) => setCss(e.target.value)} />
                 </div>
-                
+
                 <div className='width' />
-                <div className='editor-cover'>
-                    <span className='editor-title'>JavaScript</span>
+                <div className={`editor-cover ${openJs ? '' : 'js-collapsed'}`}>
+                    <div className='editor-title'>
+                        JavaScript
+                        <button
+                            type="button"
+                            className="expand-collapse-btn"
+                            onClick={() => setOpenJs(prevOpen => !prevOpen)}
+                        >
+                            <FontAwesomeIcon icon={openJs ? faCompressAlt : faExpandAlt} />
+                        </button>
+                    </div>
                     <textarea value={js} type='text' placeholder='JavaScript' className='input' onChange={(e) => setJs(e.target.value)} />
                 </div>
                 <div className='width' />
